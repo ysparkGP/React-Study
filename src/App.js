31b68b8d673ea2fb3796c87d707f249d03f079bd
Yusung <qkrdbtjd15@naver.com>
2022-02-15 23:04:33 +0900

    PropTypes 학습
    2022.02.15

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Memo from './component/Memoization/Memo';
+import Component from './component/PropTypes/Component';
+// import Example from './component/Portal/Example';
+// import ThankyouDialog from './component/Portal/ThankyouDialog';
+// import ThankyouDialog from './component/Composition/ThankyouDialog';
+// import Example from './component/Context/Example';
+// import Dialog from './component/Composition/Dialog';
+// import Memo from './component/Memoization/Memo';
 // import Input from './component/HOC/Input';
 // import Button from './component/HOC/Button';
 
 function App() {
   return (
     <div className="App">
-      <Memo></Memo>
+      <Component/>
+      {/* <Example/> */}
+      {/* <Example/> */}
+      {/* <ThankyouDialog></ThankyouDialog> */}
+      {/* <Memo></Memo> */}
       {/* <Input></Input>
       <br/>
       <br/>
